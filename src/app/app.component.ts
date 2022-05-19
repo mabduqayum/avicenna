4e5f537dd05d41480bbae745346a14b163b62224
mAbduqayum <mabduqayum@gmail.com>
2022-05-20 03:19:56 +0500

    hamuda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {Observable} from "rxjs";
 import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 import {map, shareReplay} from "rxjs/operators";
@@ -9,13 +9,22 @@ import {map, shareReplay} from "rxjs/operators";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
-
+  public getScreenWidth: any;
   constructor(private breakpointObserver: BreakpointObserver) {
   }
+
+  ngOnInit() {
+    this.onWindowResize()
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+  }
+
 }
